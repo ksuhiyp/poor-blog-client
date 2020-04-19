6b73bcf0acf2345832b849d1ba380af73c5d61f8
suhayb <ksuhiyp@gmail.com>
2020-04-19 15:18:08 +0300

    Migrate Interceptors into AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { GlobalErrorHandler } from './shared/global-error-handler';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+import { ApiInterceptor } from './shared/interceptors/api.interceptor';
+import { InternalErrorHandler } from './shared/internal-error-handler';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [CoreModule, AppRoutingModule, SharedModule],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+    { provide: ErrorHandler, useClass: GlobalErrorHandler },
+    InternalErrorHandler,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,12 @@
-import { NgModule, Optional, SkipSelf, ErrorHandler } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ApiInterceptor } from '../shared/interceptors/api.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AuthInterceptor } from '../shared/interceptors/auth.interceptor';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-import { GlobalErrorHandler } from '../shared/global-error-handler';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
@@ -22,11 +16,7 @@ import { GlobalErrorHandler } from '../shared/global-error-handler';
     }),
   ],
   exports: [],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
-    { provide: ErrorHandler, useClass: GlobalErrorHandler },
-  ],
+  providers: [],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() private parentModule: CoreModule) {
