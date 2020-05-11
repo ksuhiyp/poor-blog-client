ba8fb921746d94397078621162947470a05db45f
suhayb <ksuhiyp@gmail.com>
2020-05-12 01:57:46 +0300

    Fix shared module imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,21 @@ import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
+import { NgxFileDropModule } from 'ngx-file-drop';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
+  imports: [NgxFileDropModule,MaterialModule,CommonModule],
+  declarations: [FileUploaderComponent],
   exports: [
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    FileUploaderComponent,
   ],
 })
 export class SharedModule {}
