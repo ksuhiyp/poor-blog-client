089220953a9826e376b6ae6e02156e99b786cfcd
suhayb <ksuhiyp@gmail.com>
2020-05-20 02:32:23 +0300

    Remove comments and Center updateArticle items

--- src/app/article/create-article/create-article.component.ts
+++ src/app/article/create-article/create-article.component.ts
@@ -36,47 +36,7 @@ export class CreateArticleComponent implements OnInit {
       .subscribe();
   }
 
-  // onBodyChange(event: ChangeEvent) {
-  //   this.form.controls.body.setValue(event.editor.getData());
-  // }
-
-  // removeTag(tag) {
-  //   const selectedTagIndex = this.selectedTags.findIndex((selectedTag) => tag === selectedTag);
-  //   this.selectedTags.splice(selectedTagIndex, 1);
-  // }
-  // addTag(event) {
-  //   const term = event.value.trim();
-  //   if (term) {
-  //     this.selectedTags.filter((tag) => tag === term).length ? noop() : this.selectedTags.push(term);
-
-  //     this.tagsInput.nativeElement.value = null;
-  //     this.form.get('tagList').patchValue(this.selectedTags);
-  //   }
-  // }
-  // addSelected(event: MatAutocompleteSelectedEvent) {
-  //   const tag = event.option.value;
-  //   const term = tag.title;
-  //   this.selectedTags.filter((tag) => tag === term).length ? noop() : this.selectedTags.push(term);
-
-  //   // this.tagsCtrl.setValue(null);
-  //   this.tagsInput.nativeElement.value = null;
-  // }
-
-  // onTagListChange(): Observable<any> {
-  //   return this.form.get('tagList').valueChanges.pipe(
-  //     startWith(''),
-  //     distinctUntilChanged(),
-  //     debounceTime(50),
-  //     map((term) => (term ? (term.trim(), term.toLowerCase()) : noop())),
-  //     tap((term) => this.filteredTags.next(this.filterTagsAgainstTerm(term) || this.tags.slice()))
-  //   );
-  // }
-
-  // private filterTagsAgainstTerm(term: string) {
-  //   const regex = new RegExp(term, 'gmi');
-  //   const filteredTags = this.tags.filter((tag) => tag.title.match(regex));
-  //   return filteredTags;
-  // }
+  
 
   // private domSanitizer(url) {
   //   return this.sanitizer.bypassSecurityTrustUrl(url);
@@ -95,20 +55,5 @@ export class CreateArticleComponent implements OnInit {
   //   });
   // }
 
-  // private toFormData(form: FormGroup) {
-  //   const formData = new FormData();
-  //   // tslint:disable-next-line: forin
-  //   for (const formField in form.value) {
-  //     if (formField === 'photo') {
-  //       formData.append(formField, form.get(formField).value, form.value[formField].name);
-  //     } else if (formField === 'tagList') {
-  //       form.value[formField].forEach((tag) => {
-  //         formData.append(formField, form.value[formField]);
-  //       });
-  //     } else {
-  //       formData.append(formField, form.value[formField]);
-  //     }
-  //   }
-  //   return formData;
-  // }
+  
 }
