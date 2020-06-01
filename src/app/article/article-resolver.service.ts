6c42019bf792e65204256e4ee58d111bec88cd15
suhayb <ksuhiyp@gmail.com>
2020-06-01 23:48:25 +0300

    Cache article into router state

--- src/app/article/article-resolver.service.ts
+++ src/app/article/article-resolver.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRoute, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRoute, RouterStateSnapshot, ActivatedRouteSnapshot, Router, NavigationStart } from '@angular/router';
 import { ArticleService } from './article.service';
 import { Article } from './article';
+import { filter } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleResolver implements Resolve<Article> {
-  constructor(private service: ArticleService) {}
+  constructor(private service: ArticleService, private router: Router) {}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.service.getArticle(route.paramMap.get('id'));
+    const data = this.router.getCurrentNavigation().extras.state as Article;
+    if (data) {
+      return of(data);
+    } else if (route.paramMap.has('slug')) {
+      return this.service.getArticle(route.paramMap.get('slug'));
+    } else {
+      return this.service.getArticle(route.paramMap.get('id'));
+    }
   }
 }
--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -5,10 +5,16 @@ import { ArticleDetailComponent } from './article-detail/article-detail.componen
 import { UpdateArticleComponent } from './update-article/update-article.component';
 import { AuthenticationGuard } from '../shared/gaurds/authentication.guard';
 import { ArticleResolver } from './article-resolver.service';
+import { EditArticleGuard } from '../shared/gaurds/edit-article.guard';
 const routes: Routes = [
   { path: 'list', component: ArticlesListComponent },
-  { path: 'edit/:id', component: UpdateArticleComponent, canActivate: [AuthenticationGuard], resolve: { article: ArticleResolver } },
-  { path: ':slug', component: ArticleDetailComponent },
+  {
+    path: 'edit/:id',
+    component: UpdateArticleComponent,
+    canActivate: [AuthenticationGuard],
+    resolve: { article: ArticleResolver },
+  },
+  { path: ':slug', component: ArticleDetailComponent, resolve: { article: ArticleResolver } },
 ];
 
 @NgModule({
