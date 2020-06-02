8b5b9d4e721db1c7cd31541b0872a895fe4f0658
suhayb <ksuhiyp@gmail.com>
2020-06-03 01:35:22 +0300

    Implement Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 import { ApiInterceptor } from './shared/interceptors/api.interceptor';
 import { InternalErrorHandler } from './shared/internal-error-handler';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [CoreModule, AppRoutingModule, SharedModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
