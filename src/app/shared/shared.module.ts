887109748ccb8970558b65703aab2680a633b72c
suhayb <ksuhiyp@gmail.com>
2020-06-03 01:34:39 +0300

    Sanitize article body

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,10 +7,11 @@ import { FileUploaderComponent } from './components/file-uploader/file-uploader.
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { CommonModule } from '@angular/common';
 import { ChipsAutocompleatInputComponent } from './components/chips-autocompleat-input/chips-autocompleat-input.component';
+import { SanitizeHtmlPipe } from './pipes/sanitize-html.pipe';
 
 @NgModule({
   imports: [NgxFileDropModule, MaterialModule, CommonModule, ReactiveFormsModule],
-  declarations: [FileUploaderComponent, ChipsAutocompleatInputComponent],
+  declarations: [FileUploaderComponent, ChipsAutocompleatInputComponent, SanitizeHtmlPipe],
   exports: [
     MaterialModule,
     FlexLayoutModule,
@@ -19,6 +20,7 @@ import { ChipsAutocompleatInputComponent } from './components/chips-autocompleat
     RouterModule,
     FileUploaderComponent,
     ChipsAutocompleatInputComponent,
+    SanitizeHtmlPipe,
   ],
 })
 export class SharedModule {}
