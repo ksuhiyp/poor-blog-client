f900ae8ec381a9e906ae5640d39321934d884fd5
suhayb <ksuhiyp@gmail.com>
2020-06-03 22:06:53 +0300

    Hide add article btn when user is not loggedin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateArticleComponent } from './article/create-article/create-article.component';
-import { tap } from 'rxjs/operators';
+import { tap, share, shareReplay, map } from 'rxjs/operators';
+import { AuthService } from './auth/auth.service';
+import { Observable, of, noop } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,19 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private artivatedRoute: ActivatedRoute, private dialog: MatDialog, private router: Router) {}
+  constructor(
+    private artivatedRoute: ActivatedRoute,
+    private dialog: MatDialog,
+    private router: Router,
+    private authService: AuthService
+  ) {}
   title = 'poor-blog-client';
   isOnArticlePost;
+  isLoggedIn$: Observable<boolean>;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.isLoggedIn();
+  }
 
   openAddArticleModal() {
     const dialogRef = this.dialog.open(CreateArticleComponent, { width: '400px', data: {} });
@@ -22,9 +32,13 @@ export class AppComponent implements OnInit {
       .afterClosed()
       .pipe(
         tap(() =>
-          this.router.navigate(['/article', 'edit', dialogRef.componentInstance.article.id])
+          dialogRef.componentInstance.article ? this.router.navigate(['/article', 'edit', dialogRef.componentInstance.article.id]) : noop()
         )
       )
       .subscribe();
+  }
+
+  private isLoggedIn(): Observable<boolean> {
+    return (this.isLoggedIn$ = this.authService.isLoggedIn());
   }
 }
